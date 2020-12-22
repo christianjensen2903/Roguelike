e4020ee073ca38204504ed00b3861f7676ee5f2f
Christian <cmj290300@gmail.com>
2020-12-22 19:31:54 +0100

    Revert "Opdeling af filer"
    This reverts commit 839b98d786955c8065776a5d0963cf7585dc60d7.

--- roguelike-game.fsx
+++
@@ -1,9 +0,0 @@
-open Roguelike
-
-System.Console.Clear ()
-let canvas = Canvas (screenSizeX,screenSizeY)
-
-let menu = StartMenu canvas
-
-// Shows the class Screen as the first thing when the program starts
-menu.ClassScreen ()
